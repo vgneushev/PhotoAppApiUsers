1b0853e51cff58c750c78c970ab6d1e0e975d76b
vgneushev <vgneushev@it-one.ru>
2025-04-04 18:02:32 +0300

    add port config for lb verification

--- src/main/java/com/photoapp/api/users/photoappapiusers/ui/controllers/UserController.java
+++ src/main/java/com/photoapp/api/users/photoappapiusers/ui/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.photoapp.api.users.photoappapiusers.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/status/check")
     public String status() {
-        return "Ok";
+        return "Working on port: " + environment.getProperty("local.server.port");
     }
+
+
 
 }
